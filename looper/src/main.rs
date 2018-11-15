02af6554b80ba4d8e3cda98f1259434fa9c7501a
abhro <abhro@users.noreply.github.com>
2018-11-15 23:51:28 +0600

    Add new projects

---
+++ looper/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    let mut number = 5;
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+    println!("We have safe touchdown");
+}
