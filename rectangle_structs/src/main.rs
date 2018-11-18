d6db39c5887158ac1feacbc373974a24a8ec18df
abhro <abhro@users.noreply.github.com>
2018-11-18 21:26:26 +0600

    Update struct example

--- rectangle_structs/src/main.rs
+++ rectangle_structs/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
+    println!("rect1 is {:?}", rect1);
     println!("The area of the rectangle is {} square pixels.", area(&rect1));
 }
 
@@ -8,6 +9,7 @@ fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
