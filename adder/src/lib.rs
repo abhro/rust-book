d6f2e88dca6f16e91c2f1dd85ee13d62c7a2ab19
abhro <abhro@users.noreply.github.com>
2019-02-13 18:43:18 +0600

    Add adder unit tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,3 +1,11 @@
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -6,7 +14,15 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn another() {
         panic!("Make this test fail!");
+    }
+
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
