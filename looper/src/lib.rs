8cda5e96c75d9cc826016328c52161ab522b9e00
abhro <abhro@users.noreply.github.com>
2019-02-18 15:46:54 +0600

    Add iterator examples

---
+++ looper/src/lib.rs
@@ -0,0 +1,59 @@
+
+pub struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    pub fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        }
+        else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+
+    }
+
+    #[test]
+    fn test_sum() {
+        assert_eq!(15u32, Counter::new().sum());
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|arg| arg.0 * arg.1)
+            .filter(|x| x % 3 == 0)
+            .sum();
+
+        assert_eq!(18, sum);
+    }
+}
